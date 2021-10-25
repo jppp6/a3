15600be665cb71ad7c966a00262eb3f3832aa90f
Jp Le Blanc <6jpleblanc@gmail.com>
2021-10-25 17:53:08 -0400

    Completed assignment

--- A3_1.py
+++ A3_1.py
@@ -1,35 +1,27 @@
 # https://onq.queensu.ca/d2l/lms/dropbox/user/folder_submit_files.d2l?db=257360&grpid=0&isprv=0&bp=0&ou=578430
-
 """
-This program 
+This program uses the logic of a binary search to try to guess the users imaginary number within log2(upper_bound-lower_bound) + 1 questions.
 
 Author: Jean-Philippe Le Blanc
 Section #: 002
 Student #: 20157203
-Date: 
-
+Date: October 25, 2021
 """
 
 # main() function takes care of running the program
 def main():
-    try:
-        lower = int(input("Please enter the lower bound: ")) # Ask the user for the lower bound 
-        upper = int(input("Please enter the upper bound: "))
-    except ValueError:
-        print("Please input an integer.")
-        
+    lower = int(input("Please enter the lower bound: ")) # Ask the user for the lower bound 
+    upper = int(input("Please enter the upper bound: ")) # Ask the user for the upper bound 
 
-    while lower <= upper:
-        mid = (upper + lower)//2 
-        a = input(f"Is your number greater than {mid} (Y/N)? ")
+    while lower <= upper: # While loop that runs while lower is smaller or equal to upper
+        mid = (upper + lower)//2 # Calculate the mid point 
+        answer = input(f"Is your number greater than {mid} (Y/N)? ") # Ask the user if the number is greater than the value of mid
         
-        if a in ["y", "Y", "yes", "Yes"]:
-            lower = mid + 1
-        else: 
-            upper = mid - 1
+        if answer in ["y", "Y", "yes", "Yes"]: # If answer was yes
+            lower = mid + 1 # Change the lower bound to the value of mid + 1
+        else: # The answer was no
+            upper = mid - 1 # Change the upper bound to the value of mid - 1
 
-    print(f"The number you had in mind was {mid}")     
+    print(f"The number you had in mind was {mid}") # Print the final message with the number the user had in mind    
         
-    return 0
-
 main() # main() call to start the program

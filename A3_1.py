e4bc2367336118553bb06abbbe6b5dc546825604
Jp Le Blanc <6jpleblanc@gmail.com>
2021-10-25 14:11:55 -0400

    Added some stuff

---
+++ A3_1.py
@@ -0,0 +1,11 @@
+# https://onq.queensu.ca/d2l/lms/dropbox/user/folder_submit_files.d2l?db=257360&grpid=0&isprv=0&bp=0&ou=578430
+
+"""
+This program 
+
+Author: Jean-Philippe Le Blanc
+Section #: 002
+Student #: 20157203
+Date: 
+
+"""
